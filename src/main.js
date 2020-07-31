4a4654df69c9bc6656f91651d840588830ae83d3
tlacalv <radeon777@live.com>
2020-07-31 09:40:44 -0600

    Added mockups

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import '../node_modules/normalize.css/normalize.css';
 import "./assets/styles/main.css";
 import Vue from 'vue';
 import App from './App.vue';
-
 new Vue ({
   el: '#app',
   render: h => h(App),
